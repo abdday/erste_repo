245aefe9117d17cca77c598f74b737fca89abf8e
Abdullah Day <a11910950@unet.univie.ac.at>
2021-11-23 14:57:14 +0100

    somthing new

--- newjava.java
+++ newjava.java
@@ -7,11 +7,13 @@ public static void main(String arges[]{
 
 System.out.println(sysHello("abdo"));
 
+//ich mag tagbes
 
 }
 
 public static String sysHello(String name){
 return "hello " + name;
+//etwas anderes
 }
 
 }
